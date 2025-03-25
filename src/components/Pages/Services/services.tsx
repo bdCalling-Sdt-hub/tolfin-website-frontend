8b3cacbd5bd3c5b016aa6b68fdd663c13097b3f2
Aminulhoque01 <aminuhoque53@gmail.com>
2025-03-25 11:58:59 +0600

    services

--- src/components/Pages/Services/services.tsx
+++ src/components/Pages/Services/services.tsx
@@ -5,197 +5,35 @@ import image from '@/assets/about/pailoat.png';
 export default function Service() {
     return (
         <div className='bg-[#111111]'>
-            <div className="w-full container text-white py-16 ">
-                <h2 className="text-3xl font-bold mb-12 mt-10">
+            <div className="w-full container text-white py-16">
+                <h2 className="text-3xl font-bold mb-12 mt-10 text-center md:text-left">
                     Our Services
                 </h2>
-                <div className="w-full container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-                    <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg">
-                        <Image
-                            src={image} // Replace with actual image URL
-                            alt="Service 3"
-                            width={500}
-                            height={300}
-                            className="object-cover w-full h-64"
-                        />
-                        <div className="p-6">
-                            <h3 className="text-xl font-semibold mb-4">Security Solutions & Armed Escort Services</h3>
-                            <p className="text-sm text-gray-400 mb-4">
-                                Providing armed security for high-net-worth individuals, dignitaries, and sensitive events.
-                            </p>
-                            <div className="flex justify-center mt-4">  {/* Add margin top for spacing */}
-                                <button className="text-teal-500 px-6 py-2 rounded-lg bg-gray-700 hover:bg-gray-600 transition-colors">
-                                    Learn More
-                                </button>
+                <p className='font-small text-gray-600 py-5'>Elite Security & Travel Solutions You Can Trust.Providing seamless VIP travel, <br /> security, and protocol services with professionalism and discretion.</p>
+                <div className="w-full container mx-auto grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                    {/* Service Card */}
+                    {[...Array(9)].map((_, index) => (
+                        <div key={index} className="bg-[#154047] rounded-lg overflow-hidden shadow-lg">
+                            <Image
+                                src={image} // Replace with actual image URL
+                                alt="Service"
+                                width={500}
+                                height={300}
+                                className="object-cover w-full h-64"
+                            />
+                            <div className="p-6">
+                                <h3 className="text-xl font-semibold mb-4">Security Solutions & Armed Escort Services</h3>
+                                <p className="text-sm text-gray-400 mb-4">
+                                    Providing armed security for high-net-worth individuals, dignitaries, and sensitive events.
+                                </p>
+                                <div className="flex justify-center mt-4">
+                                    <button className="text-[#94c2c9] px-6 py-2 rounded-2xl shadow-2xl bg-[#21525a60] hover:bg-gray-600 transition-colors">
+                                        Learn More
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-
-                    <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg">
-                        <Image
-                            src={image} // Replace with actual image URL
-                            alt="Service 3"
-                            width={500}
-                            height={300}
-                            className="object-cover w-full h-64"
-                        />
-                        <div className="p-6">
-                            <h3 className="text-xl font-semibold mb-4">Security Solutions & Armed Escort Services</h3>
-                            <p className="text-sm text-gray-400 mb-4">
-                                Providing armed security for high-net-worth individuals, dignitaries, and sensitive events.
-                            </p>
-                            <div className="flex justify-center mt-4">  {/* Add margin top for spacing */}
-                                <button className="text-teal-500 px-6 py-2 rounded-lg bg-gray-700 hover:bg-gray-600 transition-colors">
-                                    Learn More
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg">
-                        <Image
-                            src={image} // Replace with actual image URL
-                            alt="Service 3"
-                            width={500}
-                            height={300}
-                            className="object-cover w-full h-64"
-                        />
-                        <div className="p-6">
-                            <h3 className="text-xl font-semibold mb-4">Security Solutions & Armed Escort Services</h3>
-                            <p className="text-sm text-gray-400 mb-4">
-                                Providing armed security for high-net-worth individuals, dignitaries, and sensitive events.
-                            </p>
-                            <div className="flex justify-center mt-4">  {/* Add margin top for spacing */}
-                                <button className="text-teal-500 px-6 py-2 rounded-lg bg-gray-700 hover:bg-gray-600 transition-colors">
-                                    Learn More
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg">
-                        <Image
-                            src={image} // Replace with actual image URL
-                            alt="Service 3"
-                            width={500}
-                            height={300}
-                            className="object-cover w-full h-64"
-                        />
-                        <div className="p-6">
-                            <h3 className="text-xl font-semibold mb-4">Security Solutions & Armed Escort Services</h3>
-                            <p className="text-sm text-gray-400 mb-4">
-                                Providing armed security for high-net-worth individuals, dignitaries, and sensitive events.
-                            </p>
-                            <div className="flex justify-center mt-4">  {/* Add margin top for spacing */}
-                                <button className="text-teal-500 px-6 py-2 rounded-lg bg-gray-700 hover:bg-gray-600 transition-colors">
-                                    Learn More
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg">
-                        <Image
-                            src={image} // Replace with actual image URL
-                            alt="Service 3"
-                            width={500}
-                            height={300}
-                            className="object-cover w-full h-64"
-                        />
-                        <div className="p-6">
-                            <h3 className="text-xl font-semibold mb-4">Security Solutions & Armed Escort Services</h3>
-                            <p className="text-sm text-gray-400 mb-4">
-                                Providing armed security for high-net-worth individuals, dignitaries, and sensitive events.
-                            </p>
-                            <div className="flex justify-center mt-4">  {/* Add margin top for spacing */}
-                                <button className="text-teal-500 px-6 py-2 rounded-lg bg-gray-700 hover:bg-gray-600 transition-colors">
-                                    Learn More
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg">
-                        <Image
-                            src={image} // Replace with actual image URL
-                            alt="Service 3"
-                            width={500}
-                            height={300}
-                            className="object-cover w-full h-64"
-                        />
-                        <div className="p-6">
-                            <h3 className="text-xl font-semibold mb-4">Security Solutions & Armed Escort Services</h3>
-                            <p className="text-sm text-gray-400 mb-4">
-                                Providing armed security for high-net-worth individuals, dignitaries, and sensitive events.
-                            </p>
-                            <div className="flex justify-center mt-4">  {/* Add margin top for spacing */}
-                                <button className="text-teal-500 px-6 py-2 rounded-lg bg-gray-700 hover:bg-gray-600 transition-colors">
-                                    Learn More
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg">
-                        <Image
-                            src={image} // Replace with actual image URL
-                            alt="Service 3"
-                            width={500}
-                            height={300}
-                            className="object-cover w-full h-64"
-                        />
-                        <div className="p-6">
-                            <h3 className="text-xl font-semibold mb-4">Security Solutions & Armed Escort Services</h3>
-                            <p className="text-sm text-gray-400 mb-4">
-                                Providing armed security for high-net-worth individuals, dignitaries, and sensitive events.
-                            </p>
-                            <div className="flex justify-center mt-4">  {/* Add margin top for spacing */}
-                                <button className="text-teal-500 px-6 py-2 rounded-lg bg-gray-700 hover:bg-gray-600 transition-colors">
-                                    Learn More
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg">
-                        <Image
-                            src={image} // Replace with actual image URL
-                            alt="Service 3"
-                            width={500}
-                            height={300}
-                            className="object-cover w-full h-64"
-                        />
-                        <div className="p-6">
-                            <h3 className="text-xl font-semibold mb-4">Security Solutions & Armed Escort Services</h3>
-                            <p className="text-sm text-gray-400 mb-4">
-                                Providing armed security for high-net-worth individuals, dignitaries, and sensitive events.
-                            </p>
-                            <div className="flex justify-center mt-4">  {/* Add margin top for spacing */}
-                                <button className="text-teal-500 px-6 py-2 rounded-lg bg-gray-700 hover:bg-gray-600 transition-colors">
-                                    Learn More
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg">
-                        <Image
-                            src={image} // Replace with actual image URL
-                            alt="Service 3"
-                            width={500}
-                            height={300}
-                            className="object-cover w-full h-64"
-                        />
-                        <div className="p-6">
-                            <h3 className="text-xl font-semibold mb-4">Security Solutions & Armed Escort Services</h3>
-                            <p className="text-sm text-gray-400 mb-4">
-                                Providing armed security for high-net-worth individuals, dignitaries, and sensitive events.
-                            </p>
-                            <div className="flex justify-center mt-4">  {/* Add margin top for spacing */}
-                                <button className="text-teal-500 px-6 py-2 rounded-lg bg-gray-700 hover:bg-gray-600 transition-colors">
-                                    Learn More
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-
-
+                    ))}
                 </div>
             </div>
         </div>
